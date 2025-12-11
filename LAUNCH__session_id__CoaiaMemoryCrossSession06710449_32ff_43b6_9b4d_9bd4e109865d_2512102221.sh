93e8856287dd9b94a147fa1e8a8429e9fc4fdbda
JGWill <jgi@jgwill.com>
2025-12-11 01:07:00 -0500

    session jgwill/coaia-spiral#6
    RELATED:
    * guillaumedescoteauxisabelle/coaia-memory#1
    
    PR:
    * guillaumedescoteauxisabelle/coaia-memory#3

---
+++ LAUNCH__session_id__CoaiaMemoryCrossSession06710449_32ff_43b6_9b4d_9bd4e109865d_2512102221.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+. _env.sh
+export SESSION_ID=a7d90b64-22ca-4a36-a356-a8400ef1b749
+export session_id__CoaiaMemoryCrossSession06710449_32ff_43b6_9b4d_9bd4e109865d_2512102221
+export session_id__CoaiaMemoryCrossSession06710449_32ff_43b6_9b4d_9bd4e109865d_2512102221__MCP_CONFIG
+export session_id__CoaiaMemoryCrossSession06710449_32ff_43b6_9b4d_9bd4e109865d_2512102221__ADD_DIR
+claude "in /src/_sessiondata/06710449-32ff-43b6-9b4d-9bd4e109865d/*jsonl are another claude-code instance captured hooks data and is what we are doing with another instance in: /a/src/coaia-memory/ in : /tmp/claude-prompt-334a0633-02f1-447f-9241-6727b5f864e9.md is the next message it will receive (so you are aware of what is going to occurs there. In a way, you are with this new generation package named coaia-spiral that was created after coaia-memory, it has a little more features and capabilities and: you would make sure that what this other instance of claude did, we have this in this new version (considering the added capabilities we have here, what you will modify might be a little different but must derived from additional values we hopefully created). 
+
+I guess that your session directory /src/_sessiondata/$SESSION_DATA/ might become the hub of so many dependent project that all depends on organizing and making this creative process adequatly working (which is always painful todo in my case.).
+
+As first step before doing anything else, I guess that analyzing what is in '/src/Miadi-18/stories/multiverse_3act_2512012121/' might be relevant because everything that we do in this session together would be an episode of that serie (maybe a meta-episode because it would implies every universe (or impact all of them )).
+
+Because '/tmp/claude-prompt-334a0633-02f1-447f-9241-6727b5f864e9.md' from the other instance will probably dissapears when I close the file, ingest it and be ready to observe what will come next in that session.
+
+ " --mcp-config $session_id__CoaiaMemoryCrossSession06710449_32ff_43b6_9b4d_9bd4e109865d_2512102221__MCP_CONFIG --add-dir $session_id__CoaiaMemoryCrossSession06710449_32ff_43b6_9b4d_9bd4e109865d_2512102221__ADD_DIR --session-id $session_id__CoaiaMemoryCrossSession06710449_32ff_43b6_9b4d_9bd4e109865d_2512102221 \
+--model sonnet
+#--permission-mode plan
